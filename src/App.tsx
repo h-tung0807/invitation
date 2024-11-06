1595e7a44afa2e4b0b891688078c7df5a54afddd
Howard Tung <h.tung@co-dx.com>
2024-11-06 15:38:49 -0700

    dinner

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { motion } from "framer-motion";
 
 function App() {
   const [showEmojis, setShowEmojis] = useState(false);
 
   const toggleEmojis = () => {
-    emailjs.init({
-      publicKey: 'BVBZ8cjcS1Azpzgaq'
-    });
-    emailjs.send('service_p62y7os', 'template_zbq8wt5').then((response) => {
-      
-        alert('Email sent successfully!');
-      
-    });
+    
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={toggleEmojis}>
-          {showEmojis ? 'Hide Emojis' : 'Show Emojis'}
-        </button>
-        {showEmojis && (
-          <div className="emoji-layer">
-            <span role="img" aria-label="smile">😊</span>
-            <span role="img" aria-label="heart">❤️</span>
-            <span role="img" aria-label="thumbs up">👍</span>
-          </div>
-        )}
-      </header>
+    <div className="blurred-background">
+    <div className="centered-content">
+      <img src="https://media1.tenor.com/m/whAhQj8irG8AAAAd/lady-and-the-tramp-dogs.gif"/>
     </div>
+  </div>
+    // <div className="App">
+    //   <img className="top-image" src="https://media1.tenor.com/m/whAhQj8irG8AAAAd/lady-and-the-tramp-dogs.gif" alt="this slowpoke moves" width="250" />
+    //   <div style={{
+    //     display: 'grid',
+    //     placeContent: 'center',
+    //     backgroundColor: 'lightblue',
+    //   }}>
+    //     <motion.button
+    //       className="box"
+    //       whileHover={{ scale: [null, 1.5, 1.4] }}
+    //       transition={{ duration: 0.3 }}
+    //       style={{
+    //         backgroundColor: 'black',
+    //       }}
+    //     />
+    //   </div>
+    // </div>
   );
 }
 
